f95803debdfc08488abcb3efe5b8d54d434a5961
CTTZO <fanyuntian@gmail.com>
2021-07-14 16:24:45 -0400

    Added Recovered

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
 import requests
 import tkinter as tk
+
 response = requests.get("https://api.covid19api.com/summary")
 w = tk.Tk()
 w.geometry("800x400")
 w.title("Covid Stats")
-for i in response.json()["Countries"]:
-    if i["Country"] == "Canada":
-        print(i)
+
+
+def show_data(cn):
+    for i in response.json()["Countries"]:
+        if i["Country"] == cn:
+            cal = tk.Label(w, font="Arial 20", text=cn)
+            cal.pack()
+            tc = i["TotalConfirmed"]
+            tcl = tk.Label(w, text="Total Confirmed Cases: " + str(tc))
+            tcl.pack()
+            tr = i["TotalRecovered"]
+            trl = tk.Label(w, text="Total Recovered Cases: " + str(tr))
+            trl.pack()
+            nc = i["NewConfirmed"]
+            ncl = tk.Label(w, text="New Confirmed Cases: " + str(nc))
+            ncl.pack()
+            nr = i["TotalRecovered"]
+            nrl = tk.Label(w, text="New Recovered Cases: " + str(nr))
+            nrl.pack()
+
+
+ci = input("Input A Country Name: ")
+show_data(ci)
+print("Thank you for your input. Check your taskbar. When you find the window named Covid Stats, \nclick it. if "
+      "it is "
+      "blank, it means your country name is invalid.")
 w.mainloop()
